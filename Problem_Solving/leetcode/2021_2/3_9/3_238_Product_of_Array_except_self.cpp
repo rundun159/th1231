11c6f603cb91eb9800153e71555951ec2973ae27
rundun159 <xogud1231@gmail.com>
2021-02-10 22:38:39 +0900

    21.2.10 237 Product

--- Problem_Solving/leetcode/2021_2/3_9/3_238_Product_of_Array_except_self.cpp
+++ Problem_Solving/leetcode/2021_2/3_9/3_238_Product_of_Array_except_self.cpp
@@ -8,7 +8,14 @@ class Solution {
 public:
     static vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        
+        vector<int> left_product(n,1), right_product(n,1), ret(n,1);
+        for(int i=1;i<n;i++)
+            left_product[i] = left_product[i-1] * nums[i-1];
+        for(int i=n-2;i>=0;i--)
+            right_product[i] = right_product[i+1] * nums[i+1];
+        for(int i=0;i<n;i++)
+            ret[i] = left_product[i] * right_product[i];
+        return ret;
     }
 };
 int main()
